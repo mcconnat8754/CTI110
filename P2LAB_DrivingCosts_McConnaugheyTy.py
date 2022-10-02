be20c0203e4848986bce20270a5c2beae8f3fd81
mcconnat8754 <114018360+mcconnat8754@users.noreply.github.com>
2022-10-01 21:13:39 -0400

    uplaoded

---
+++ P2LAB_DrivingCosts_McConnaugheyTy.py
@@ -0,0 +1,6 @@
+mileage = float(input())
+cost = float(input())
+price1 = (20 / mileage) * cost
+price2 = (75 / mileage) * cost
+price3 = (500 / mileage) * cost
+print(f'{price1:.2f} {price2:.2f} {price3:.2f}')
